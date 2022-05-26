7cba5966af45fd7c2c844739fa81b16b5b5cc601
dhaparam <106304715+dhaparam@users.noreply.github.com>
2022-05-26 13:39:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,127 +1,128 @@
-import os
-import sys
-import uwsgiconfig as uc
-import shutil
+import os.path
+from distutils.command.build import build as orig_build
+from distutils.core import Command
 
+from setuptools import Distribution
+from setuptools import Extension
 from setuptools import setup
-from setuptools.dist import Distribution
-from setuptools.command.install import install
-from setuptools.command.install_lib import install_lib
-from setuptools.command.build_ext import build_ext
+from setuptools.command.install import install as orig_install
+
 
 try:
-    from wheel.bdist_wheel import bdist_wheel
-    HAS_WHEEL = True
-except ImportError:
-    HAS_WHEEL = False
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
-"""
-This is a hack allowing you installing
-uWSGI and uwsgidecorators via pip and easy_install
-since 1.9.11 it automatically detects pypy
-"""
+    class bdist_wheel(_bdist_wheel):
 
-uwsgi_compiled = False
-
-
-def get_profile():
-    is_pypy = False
-    try:
-        import __pypy__
-        is_pypy = True
-    except ImportError:
-        pass
-    if is_pypy:
-        profile = os.environ.get('UWSGI_PROFILE', 'buildconf/pypy.ini')
-    else:
-        profile = os.environ.get('UWSGI_PROFILE', 'buildconf/default.ini')
-    if not profile.endswith('.ini'):
-        profile = "%s.ini" % profile
-    if not '/' in profile:
-        profile = "buildconf/%s" % profile
-
-    return profile
-
-
-def patch_bin_path(cmd, conf):
-
-    bin_name = conf.get('bin_name')
-
-    if not os.path.isabs(bin_name):
-        print('Patching "bin_name" to properly install_scripts dir')
-        try:
-            if not os.path.exists(cmd.install_scripts):
-                os.makedirs(cmd.install_scripts)
-            conf.set('bin_name',
-                     os.path.join(cmd.install_scripts, conf.get('bin_name')))
-        except:
-            conf.set('bin_name', sys.prefix + '/bin/' + bin_name)
-
-
-class uWSGIBuilder(build_ext):
-
-    def run(self):
-        global uwsgi_compiled
-        if not uwsgi_compiled:
-            conf = uc.uConf(get_profile())
-            patch_bin_path(self, conf)
-            uc.build_uwsgi(conf)
-            uwsgi_compiled = True
-
-
-class uWSGIInstall(install):
-
-    def run(self):
-        global uwsgi_compiled
-        if not uwsgi_compiled:
-            conf = uc.uConf(get_profile())
-            patch_bin_path(self, conf)
-            uc.build_uwsgi(conf)
-            uwsgi_compiled = True
-        install.run(self)
-
-
-class uWSGIInstallLib(install_lib):
-
-    def run(self):
-        global uwsgi_compiled
-        if not uwsgi_compiled:
-            conf = uc.uConf(get_profile())
-            patch_bin_path(self, conf)
-            uc.build_uwsgi(conf)
-            uwsgi_compiled = True
-        install_lib.run(self)
-
-
-if HAS_WHEEL:
-    class uWSGIWheel(bdist_wheel):
         def finalize_options(self):
-            bdist_wheel.finalize_options(self)
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
             self.root_is_pure = False
 
-
-class uWSGIDistribution(Distribution):
-
-    def __init__(self, *attrs):
-        Distribution.__init__(self, *attrs)
-        self.cmdclass['install'] = uWSGIInstall
-        self.cmdclass['install_lib'] = uWSGIInstallLib
-        self.cmdclass['build_ext'] = uWSGIBuilder
-        if HAS_WHEEL:
-            self.cmdclass['bdist_wheel'] = uWSGIWheel
-
-    def is_pure(self):
-        return False
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't contain any python source
+            python, abi = 'py2.py3', 'none'
+            return python, abi, plat
+except ImportError:
+    bdist_wheel = None
 
 
-setup(name='uWSGI',
-      version=uc.uwsgi_version,
-      description='The uWSGI server',
-      author='Unbit',
-      author_email='info@unbit.it',
-      license='GPL2',
-      descriptions='The uWSGI Platform',
-      url='https://uwsgi-docs.readthedocs.io/en/latest/',
-      py_modules=['uwsgidecorators'],
-      distclass=uWSGIDistribution,
-     )
+class ExeDistribution(Distribution):
+    c_executables = ()
+
+
+class build(orig_build):
+    sub_commands = orig_build.sub_commands + [
+        ('build_cexe', None),
+    ]
+
+
+class install(orig_install):
+    sub_commands = orig_install.sub_commands + [
+        ('install_cexe', None),
+    ]
+
+
+class install_cexe(Command):
+    description = 'install C executables'
+    outfiles = ()
+
+    def initialize_options(self):
+        self.build_dir = self.install_dir = None
+
+    def finalize_options(self):
+        # this initializes attributes based on other commands' attributes
+        self.set_undefined_options('build', ('build_scripts', 'build_dir'))
+        self.set_undefined_options(
+            'install', ('install_scripts', 'install_dir'))
+
+    def run(self):
+
+        self.outfiles = self.copy_tree(self.build_dir, self.install_dir)
+
+    def get_outputs(self):
+        return self.outfiles
+
+
+class build_cexe(Command):
+    description = 'build C executables'
+
+    def initialize_options(self):
+        self.build_scripts = None
+        self.build_temp = None
+
+    def finalize_options(self):
+        self.set_undefined_options(
+            'build',
+            ('build_scripts', 'build_scripts'),
+            ('build_temp', 'build_temp'),
+        )
+
+    def run(self):
+        # stolen and simplified from distutils.command.build_ext
+        from distutils.ccompiler import new_compiler
+
+        compiler = new_compiler(verbose=True)
+
+        for exe in self.distribution.c_executables:
+            objects = compiler.compile(
+                exe.sources,
+                output_dir=self.build_temp,
+            )
+            compiler.link_executable(
+                objects,
+                exe.name,
+                output_dir=self.build_scripts,
+                extra_postargs=exe.extra_link_args,
+            )
+
+    def get_outputs(self):
+        return [
+            os.path.join(self.build_scripts, exe.name)
+            for exe in self.distribution.c_executables
+        ]
+
+
+setup(
+    name='dumb-init',
+    description='Simple wrapper script which proxies signals to a child',
+    version=open('VERSION').read().strip(),
+    author='Yelp',
+    url='https://github.com/Yelp/dumb-init/',
+    platforms='linux',
+    c_executables=[
+        Extension(
+            'dumb-init',
+            ['dumb-init.c'],
+            extra_link_args=['-static'],
+        ),
+    ],
+    cmdclass={
+        'bdist_wheel': bdist_wheel,
+        'build': build,
+        'build_cexe': build_cexe,
+        'install': install,
+        'install_cexe': install_cexe,
+    },
+    distclass=ExeDistribution,
+)
