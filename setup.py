e4e8157c0bc9c0e455c50759ddd41b9cc6154666
dhaparam <106304715+dhaparam@users.noreply.github.com>
2022-05-30 14:55:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,128 +1,180 @@
-import os.path
-from distutils.command.build import build as orig_build
-from distutils.core import Command
+# encoding: utf-8
 
-from setuptools import Distribution
-from setuptools import Extension
+"""
+This is a hack allowing you installing
+uWSGI and uwsgidecorators via pip and easy_install
+since 1.9.11 it automatically detects pypy
+"""
+
+import os
+import sys
+import errno
+import shlex
+import uwsgiconfig
+
 from setuptools import setup
-from setuptools.command.install import install as orig_install
+from setuptools.command.build_ext import build_ext
+from distutils.core import Extension
 
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+class uWSGIBuildExt(build_ext):
 
-    class bdist_wheel(_bdist_wheel):
+    UWSGI_NAME = 'pyuwsgi'
+    UWSGI_PLUGIN = 'pyuwsgi'
+    UWSGI_PROFILE = 'pyuwsgi'
 
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            # Mark us as not a pure python package
-            self.root_is_pure = False
+    def build_extensions(self):
+        self.uwsgi_setup()
+        # XXX: needs uwsgiconfig fix
+        self.uwsgi_build()
+        if 'UWSGI_USE_DISTUTILS' not in os.environ:
+            # XXX: needs uwsgiconfig fix
+            # uwsgiconfig.build_uwsgi(self.uwsgi_config)
+            return
 
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            # We don't contain any python source
-            python, abi = 'py2.py3', 'none'
-            return python, abi, plat
-except ImportError:
-    bdist_wheel = None
+        else:
+            # XXX: needs uwsgiconfig fix
+            os.unlink(self.uwsgi_config.get('bin_name'))
+
+        # FIXME: else build fails :(
+        for baddie in set(self.compiler.compiler_so) & set(('-Wstrict-prototypes',)):
+            self.compiler.compiler_so.remove(baddie)
+
+        build_ext.build_extensions(self)
+
+    def uwsgi_setup(self):
+        profile = os.environ.get('UWSGI_PROFILE') or 'buildconf/%s.ini' % self.UWSGI_PROFILE
+
+        if not profile.endswith('.ini'):
+            profile = profile + '.ini'
+        if '/' not in profile:
+            profile = 'buildconf/' + profile
+
+        # FIXME: update uwsgiconfig to properly set _EVERYTHING_!
+        config = uwsgiconfig.uConf(profile)
+        # insert in the beginning so UWSGI_PYTHON_NOLIB is exported
+        # before the python plugin compiles
+        ep = config.get('embedded_plugins').split(',')
+        if self.UWSGI_PLUGIN in ep:
+            ep.remove(self.UWSGI_PLUGIN)
+        ep.insert(0, self.UWSGI_PLUGIN)
+        config.set('embedded_plugins', ','.join(ep))
+        config.set('as_shared_library', 'true')
+        config.set('bin_name', self.get_ext_fullpath(self.UWSGI_NAME))
+        try:
+            os.makedirs(os.path.dirname(config.get('bin_name')))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        self.uwsgi_profile = profile
+        self.uwsgi_config = config
+
+    def uwsgi_build(self):
+        uwsgiconfig.build_uwsgi(self.uwsgi_config)
+
+        # XXX: merge uwsgi_setup (see other comments)
+        for ext in self.extensions:
+            if ext.name == self.UWSGI_NAME:
+                ext.sources = [s + '.c' for s in self.uwsgi_config.gcc_list]
+                ext.library_dirs = self.uwsgi_config.include_path[:]
+                ext.libraries = list()
+                ext.extra_compile_args = list()
+
+                for x in uwsgiconfig.uniq_warnings(
+                    self.uwsgi_config.ldflags + self.uwsgi_config.libs,
+                ):
+                    for y in shlex.split(x):
+                        if y.startswith('-l'):
+                            ext.libraries.append(y[2:])
+                        elif y.startswith('-L'):
+                            ext.library_dirs.append(y[2:])
+
+                for x in self.uwsgi_config.cflags:
+                    for y in shlex.split(x):
+                        if y:
+                            ext.extra_compile_args.append(y)
 
 
-class ExeDistribution(Distribution):
-    c_executables = ()
+LONG_DESCRIPTION = """
+# The uWSGI server as a Python module
 
+## Install
 
-class build(orig_build):
-    sub_commands = orig_build.sub_commands + [
-        ('build_cexe', None),
-    ]
+```
+pip install pyuwsgi
+```
 
+## Run
 
-class install(orig_install):
-    sub_commands = orig_install.sub_commands + [
-        ('install_cexe', None),
-    ]
+The installed script, `pyuwsgi`, is a drop-in replacement for the `uwsgi` script.
 
+You can also call it directly in your Python code with a list of valid uWSGI options:
 
-class install_cexe(Command):
-    description = 'install C executables'
-    outfiles = ()
+```python
+import pyuwsgi
+pyuwsgi.run(["--help"])
+```
 
-    def initialize_options(self):
-        self.build_dir = self.install_dir = None
+## Differences from uWSGI
 
-    def finalize_options(self):
-        # this initializes attributes based on other commands' attributes
-        self.set_undefined_options('build', ('build_scripts', 'build_dir'))
-        self.set_undefined_options(
-            'install', ('install_scripts', 'install_dir'))
+This is built from uWSGI's source without any modifications.
+A different [`setup.py`](https://github.com/unbit/uwsgi/blob/uwsgi-2.0/setup.pyuwsgi.py)
+is used to make the project a friendlier part of the Python ecosystem. It allows it
+to be imported as a Python module and distributed using the
+[wheel format](https://www.python.org/dev/peps/pep-0427/).
 
-    def run(self):
+The full uWSGI documentation can be found at
+[https://uwsgi-docs.readthedocs.org](https://uwsgi-docs.readthedocs.org).
 
-        self.outfiles = self.copy_tree(self.build_dir, self.install_dir)
+---
 
-    def get_outputs(self):
-        return self.outfiles
+[![Lincoln Loop](https://cldup.com/gyNz5rfTkR.png)](https://lincolnloop.com)
 
+`pyuwsgi` is sponsored by [Lincoln Loop](https://lincolnloop.com).
 
-class build_cexe(Command):
-    description = 'build C executables'
+[![Unbit](https://cldup.com/TTNag1Zlcw.png)](http://unbit.com/)
 
-    def initialize_options(self):
-        self.build_scripts = None
-        self.build_temp = None
+`uwsgi` is the creation of [Unbit](http://unbit.com/).
 
-    def finalize_options(self):
-        self.set_undefined_options(
-            'build',
-            ('build_scripts', 'build_scripts'),
-            ('build_temp', 'build_temp'),
-        )
-
-    def run(self):
-        # stolen and simplified from distutils.command.build_ext
-        from distutils.ccompiler import new_compiler
-
-        compiler = new_compiler(verbose=True)
-
-        for exe in self.distribution.c_executables:
-            objects = compiler.compile(
-                exe.sources,
-                output_dir=self.build_temp,
-            )
-            compiler.link_executable(
-                objects,
-                exe.name,
-                output_dir=self.build_scripts,
-                extra_postargs=exe.extra_link_args,
-            )
-
-    def get_outputs(self):
-        return [
-            os.path.join(self.build_scripts, exe.name)
-            for exe in self.distribution.c_executables
-        ]
-
+"""
 
 setup(
-    name='dumb-init',
-    description='Simple wrapper script which proxies signals to a child',
-    #version=open('VERSION').read().strip(),
-    author='Yelp',
-    url='https://github.com/Yelp/dumb-init/',
-    platforms='linux',
-    c_executables=[
-        Extension(
-            'dumb-init',
-            ['dumb-init.c'],
-            extra_link_args=['-static'],
-        ),
-    ],
+    name='pyuwsgi',
+    license='GPL2',
+    version=uwsgiconfig.uwsgi_version,
+    author='Unbit',
+    author_email='info@unbit.it',
+    description='The uWSGI server',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     cmdclass={
-        'bdist_wheel': bdist_wheel,
-        'build': build,
-        'build_cexe': build_cexe,
-        'install': install,
-        'install_cexe': install_cexe,
-    },
-    distclass=ExeDistribution,
-)
+        'build_ext': uWSGIBuildExt,
+        },
+    py_modules=[
+        'uwsgidecorators',
+        ],
+    ext_modules=[
+        Extension('pyuwsgi', sources=[]),
+        ],
+    entry_points={
+        'console_scripts': ['pyuwsgi=pyuwsgi:run'],
+        },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        ]
+    )
